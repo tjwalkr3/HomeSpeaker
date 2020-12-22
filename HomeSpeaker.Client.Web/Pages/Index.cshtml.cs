713ac882f9789fb01598a6bbf69e0985a5096d71
Jonathan Allen <jallen@snow.edu>
2020-12-22 15:11:02 -0700

    update web client dockerfile to build arm64; localdeploy to use version numbers

--- HomeSpeaker.Client.Web/Pages/Index.cshtml.cs
+++ HomeSpeaker.Client.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeSpeaker.Client;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,14 +12,17 @@ namespace HomeSpeaker.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly HomeSpeakerClient homeSpeakerClient;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            HomeSpeakerClient homeSpeakerClient)
         {
+            this.homeSpeakerClient = homeSpeakerClient;
         }
 
         public void OnGet()
         {
-
+            
         }
     }
 }
--- scripts/localdeploy.ps1
+++ scripts/localdeploy.ps1
@@ -3,15 +3,24 @@
 # incapable of building an arm64 image that works.
 # ...or at least I'm incapable of making them do so.
 
-write-host "** Building Image **"
-docker build -t snowjallen/homespeakerserver -f ./HomeSpeaker.Server/Dockerfile .
+$version = 0;
+if(test-path "version.txt") {
+	$version = [int](get-content "version.txt")
+}
+$version++ | set-content "version.txt"
+$tag = "v$version"
 
-write-host "** Pushing Image **"
-docker push snowjallen/homespeakerserver
+write-host "** Building Images (tag $tag) **"
+docker build -t snowjallen/homespeakerserver:$tag -f ./HomeSpeaker.Server/Dockerfile .
+docker build -t snowjallen/homespeakerwebclient:$tag -f ./HomeSpeaker.Client.Web/Dockerfile .
+
+write-host "** Pushing Images (tag $tag)**"
+docker push snowjallen/homespeakerserver:$tag
+docker push snowjallen/homespeakerwebclient:$tag
 
 write-host "** Copy latest restart.ps1 and docker-compose.yml files **"
 scp scripts/restart.ps1 pi@192.168.1.20:~/scripts/
 scp scripts/docker-compose.yml pi@192.168.1.20:~/scripts/
 
-write-host "** Restarting docker-compose on rpi4 **"
-ssh rpi4 'cd scripts && pwsh ./restart.ps1 -tag latest'
+write-host "** Restarting docker-compose on rpi4  (tag $tag) **"
+ssh rpi4 "cd scripts && pwsh ./restart.ps1 -tag $tag"
