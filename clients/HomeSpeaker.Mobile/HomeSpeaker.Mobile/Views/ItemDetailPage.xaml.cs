d85dc378e1c9ddca325811d8b448b65f11e24df6
Jonathan Allen <jallen@snow.edu>
2021-01-04 17:05:44 -0700

    Add xamarin.forms client with proof-of-concept grpc support.

---
+++ clients/HomeSpeaker.Mobile/HomeSpeaker.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HomeSpeaker.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HomeSpeaker.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
