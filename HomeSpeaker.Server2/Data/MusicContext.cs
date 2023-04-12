91d22be28c5f1b8d5fbc81cdbf8b71a9af9f87d9
Jonathan Allen <jallen@snow.edu>
2023-04-12 16:44:01 -0600

    more work on playlists

--- HomeSpeaker.Server2/Data/MusicContext.cs
+++ HomeSpeaker.Server2/Data/MusicContext.cs
@@ -26,7 +26,7 @@ public class Playlist
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<PlaylistItem> Songs { get; set; }
+    public List<PlaylistItem> Songs { get; set; } = new();
 }
 
 public class PlaylistItem
