296a5702d06afb64ef672b6b04b2dfb3af82cc26
Jonathan Allen <jallen@snow.edu>
2023-04-14 16:52:55 -0600

    first try at using mudblazor to add drag and drop...works on mobile, but scrolling doesn't.

--- HomeSpeaker.WebAssembly/Program.cs
+++ HomeSpeaker.WebAssembly/Program.cs
@@ -3,6 +3,7 @@ using HomeSpeaker.WebAssembly.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Fast.Components.FluentUI;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<HomeSpeakerService>();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
