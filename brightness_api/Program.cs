bcbd75054897aa6a52bdcd421f25c56fb76cc9ce
Jonathan Allen <jallen@snow.edu>
2023-10-01 23:08:52 -0600

    enforce valid ranges

--- brightness_api/Program.cs
+++ brightness_api/Program.cs
@@ -9,8 +9,15 @@ app.MapGet("/", () => "Brightness API.");
 
 app.MapGet("/set", (int brightness, ILogger<Program> logger) =>
 {
+    if(brightness < 0 || brightness > 255)
+    {
+        logger.LogWarning("Brightness of {brightness} is outside of 0-255 range.", brightness);
+        return "Brightness must be 0-255";
+    }
+
     logger.LogInformation("Setting brightness to {brightness}", brightness);
     Process.Start("sudo", $"bash -c \"echo {brightness} > /sys/class/backlight/10-0045/brightness");
+    return $"Set brightness to {brightness}";
 });
 
 app.Run();
