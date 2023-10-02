22e2bef22c2e6d69e7794366edf8a03d8aeb7787
Jonathan Allen <jallen@snow.edu>
2023-10-01 23:01:51 -0600

    make the logger a bit more explicit

--- brightness_api/Program.cs
+++ brightness_api/Program.cs
@@ -7,10 +7,12 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Brightness API.");
 
-app.MapGet("/set", (int brightness, ILogger logger) =>
+app.MapGet("/set", (int brightness, ILogger<Program> logger) =>
 {
     logger.LogInformation("Setting brightness to {brightness}", brightness);
     Process.Start("sudo", $"bash -c \"echo {brightness} > /sys/class/backlight/10-0045/brightness");
 });
 
 app.Run();
+
+public partial class Program { }
