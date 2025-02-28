8f2ef595e81bb562b914e928e1d2afdf355b9535
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-02-27 20:39:24 -0700

    got 4 basic viewmodel tests to pass

---
+++ HomeSpeaker.MauiTests/MainPageViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using HomeSpeaker.Maui.ViewModels;
+using HomeSpeaker.Maui.Services;
+using NSubstitute;
+namespace HomeSpeaker.MauiTests;
+
+public class MainPageViewModelTest
+{
+    [Fact]
+    public void CreateViewModel()
+    {
+        // Arrange
+        PlayerContext context = new();
+
+        // Act
+        var viewModel = new MainPageViewModel(context);
+
+        // Assert
+        Assert.NotNull(viewModel);
+    }
+}
---
+++ HomeSpeaker.MauiTests/PlaylistPageViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using HomeSpeaker.Maui.ViewModels;
+using HomeSpeaker.Maui.Services;
+using NSubstitute;
+namespace HomeSpeaker.MauiTests;
+
+public class PlaylistPageViewModelTest
+{
+    [Fact]
+    public void CreateViewModel()
+    {
+        // Arrange
+        PlayerContext context = new();
+
+        // Act
+        var viewModel = new PlaylistPageViewModel(context);
+
+        // Assert
+        Assert.NotNull(viewModel);
+    }
+}
